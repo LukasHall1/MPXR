3e9be042304e3f96ba659076163b3db6f205ba23
LukasHall <lukashall@u.boisestate.edu>
2026-03-28 17:04:40 -0600

    painting in the air

---
+++ MiniPaintingXR/Assets/XRPainter.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class XRPainter : MonoBehaviour
+{
+    public InputActionProperty triggerAction;
+    public GameObject strokePrefab;
+    public Transform tip;
+    public Color currentColor;
+
+    public float minDistance = 0.01f;
+
+    private LineRenderer currentLine;
+    private List<Vector3> points = new List<Vector3>();
+    private bool isPainting = false;
+
+
+    public InputActionReference colorButton;
+
+    private Color[] colors = new Color[]
+    {
+        Color.red,
+        Color.blue,
+        Color.green,
+        Color.yellow,
+        Color.magenta,
+        Color.white,
+        Color.black
+    };
+
+    private int colorIndex = 0;
+
+    void Update()
+    {
+        float trigger = triggerAction.action.ReadValue<float>();
+
+        if (colorButton.action.WasPressedThisFrame())
+        {
+            colorIndex = (colorIndex + 1) % colors.Length;
+            currentColor = colors[colorIndex];
+        }
+
+        if (trigger > 0.1f)
+        {
+            if (!isPainting)
+                StartStroke();
+
+            AddPoint();
+        }
+        else
+        {
+            if (isPainting)
+                EndStroke();
+        }
+    }
+
+    void StartStroke()
+    {
+        isPainting = true;
+
+        GameObject stroke = Instantiate(strokePrefab);
+        currentLine = stroke.GetComponent<LineRenderer>();
+
+        // FORCE unique material instance
+        currentLine.material = new Material(currentLine.material);
+
+        // Apply color
+        currentLine.material.color = currentColor;
+
+        // Optional (still useful)
+        currentLine.startColor = currentColor;
+        currentLine.endColor = currentColor;
+
+        points.Clear();
+    }
+    
+
+    void AddPoint()
+    {
+        Vector3 position = tip.position;
+
+        if (points.Count == 0 || Vector3.Distance(points[^1], position) > minDistance)
+        {
+            points.Add(position);
+
+            currentLine.positionCount = points.Count;
+            currentLine.SetPositions(points.ToArray());
+        }
+    }
+
+    void EndStroke()
+    {
+        isPainting = false;
+        currentLine = null;
+    }
+}
